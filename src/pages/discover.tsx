caba07d863f69c1bccdb3ef29f8cec139b11f286
Amanda Forrester <amanda.forrester1@gmail.com>
2023-05-07 22:30:39 -0500

    Added info to new pages

---
+++ src/pages/discover.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DiscoverPage = () => {
+  // Your page content here
+};
+
+export default DiscoverPage;
---
+++ src/pages/findbychords.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FindByChordsPage = () => {
+  // Your page content here
+};
+
+export default FindByChordsPage;
